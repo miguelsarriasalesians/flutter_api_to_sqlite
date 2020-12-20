0c0643532c921892138ef9ce944f935c6645299f
Miguel Peña <mpena@universaldoctor.com>
2020-12-20 16:51:11 +0100

    Creado widget FormattedRow para formatear los campos

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:api_to_sqlite_flutter/src/providers/db_provider.dart';
 import 'package:api_to_sqlite_flutter/src/providers/programmer_api_provider.dart';
 import 'package:flutter/material.dart';
 
+const double spaceBetweenRows = 10;
+
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
 
@@ -43,7 +45,7 @@ class _HomePageState extends State<HomePage> {
           ? Center(
               child: CircularProgressIndicator(),
             )
-          : _buildEmployeeListView(),
+          : _buildProgrammerListView(),
     );
   }
 
@@ -77,10 +79,10 @@ class _HomePageState extends State<HomePage> {
       isLoading = false;
     });
 
-    print('All employees deleted');
+    print('All programmers deleted');
   }
 
-  _buildEmployeeListView() {
+  _buildProgrammerListView() {
     return FutureBuilder(
       future: DBProvider.db.getAllProgrammers(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
@@ -89,25 +91,115 @@ class _HomePageState extends State<HomePage> {
             child: CircularProgressIndicator(),
           );
         } else {
-          return ListView.separated(
-            separatorBuilder: (context, index) => Divider(
-              color: Colors.black12,
+          return Container(
+            margin: EdgeInsets.only(top: 10),
+            child: ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      color: Color(0xffdce6e0),
+                      borderRadius: BorderRadius.circular(15)),
+                  child: ListTile(
+                    leading: Text("${index + 1}",
+                        style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22)),
+                    title: Column(
+                      children: [
+                        FormattedRow(
+                          snapshot: snapshot,
+                          index: index,
+                          title: "Name",
+                          value: snapshot.data[index].firstName,
+                        ),
+                        SizedBox(
+                          height: spaceBetweenRows,
+                        ),
+                        FormattedRow(
+                          snapshot: snapshot,
+                          index: index,
+                          title: "Surname",
+                          value: snapshot.data[index].lastName,
+                        ),
+                        SizedBox(
+                          height: spaceBetweenRows,
+                        ),
+                        FormattedRow(
+                          snapshot: snapshot,
+                          index: index,
+                          title: "Email",
+                          value: snapshot.data[index].email,
+                        ),
+                        SizedBox(
+                          height: spaceBetweenRows,
+                        ),
+                        FormattedRow(
+                          snapshot: snapshot,
+                          index: index,
+                          title: "Technology",
+                          value: snapshot.data[index].technologies,
+                        ),
+                        SizedBox(
+                          height: spaceBetweenRows,
+                        ),
+                        FormattedRow(
+                          snapshot: snapshot,
+                          index: index,
+                          title: "Years of experience",
+                          value:
+                              snapshot.data[index].yearsExperience.toString(),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+                // return ListTile(
+                //   leading: Text(
+                //     "${index + 1}",
+                //     style: TextStyle(fontSize: 20.0),
+                //   ),
+                //   title: Text(
+                //       "Name: ${snapshot.data[index].firstName} ${snapshot.data[index].lastName} "),
+                //   subtitle: Text('EMAIL: ${snapshot.data[index].email}'),
+                // );
+              },
             ),
-            itemCount: snapshot.data.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                leading: Text(
-                  "${index + 1}",
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                title: Text(
-                    "Name: ${snapshot.data[index].firstName} ${snapshot.data[index].lastName} "),
-                subtitle: Text('EMAIL: ${snapshot.data[index].email}'),
-              );
-            },
           );
         }
       },
+    );
+  }
+}
+
+class FormattedRow extends StatelessWidget {
+  final AsyncSnapshot snapshot;
+  final int index;
+  final String title;
+  final String value;
+
+  FormattedRow({this.snapshot, this.index, this.title, this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          "$title: ",
+          style: TextStyle(
+              color: Colors.black54, fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        Text(
+          value,
+          style: TextStyle(
+              color: Colors.black54,
+              fontWeight: FontWeight.normal,
+              fontSize: 18),
+        ),
+      ],
     );
   }
 }
